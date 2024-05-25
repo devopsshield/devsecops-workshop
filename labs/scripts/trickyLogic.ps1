e9a263bc8ccfb94c44046b494160e26a7fba332d
Emmanuel.Knafo <Emmanuel.Knafo@cad4devops.com>
2024-05-25 15:38:04 -0400

    try this

--- labs/scripts/trickyLogic.ps1
+++ labs/scripts/trickyLogic.ps1
@@ -1,22 +1,25 @@
-$DEFEECTDOJO_TOKEN = "token"
-$DEFEECTDOJO_COMMONPASSWORD = "password"
+#$DEFEECTDOJO_TOKEN = "token"
+#$DEFEECTDOJO_COMMONPASSWORD = "password"
 
-if ($DEFEECTDOJO_TOKEN) {
-    echo "DEFECTDOJO_TOKEN is set"
+$defectDojoTokenIsSet = $null -ne $DEFEECTDOJO_TOKEN -and $DEFEECTDOJO_TOKEN -ne ""
+$defectDojoCommonPasswordIsSet = $null -ne $DEFEECTDOJO_COMMONPASSWORD -and $DEFEECTDOJO_COMMONPASSWORD -ne ""
+
+if ($defectDojoTokenIsSet) {
+    Write-Host "DEFECTDOJO_TOKEN is set"
     exit 0
 }
 else {    
-    echo "Warning: DEFEECTDOJO_TOKEN is not set. Please set the secret in the dev environment."
-    echo "checking for DEFECTDOJO_COMMONPASSWORD"
-    if ($DEFEECTDOJO_COMMONPASSWORD) {
-        echo "DEFECTDOJO_COMMONPASSWORD is set"
-        echo "Trying to get token from common user Student000..."
+    Write-Host "Warning: DEFEECTDOJO_TOKEN is not set. Please set the secret in the dev environment."
+    Write-Host "checking for DEFECTDOJO_COMMONPASSWORD"
+    if ($defectDojoCommonPasswordIsSet) {
+        Write-Host "DEFECTDOJO_COMMONPASSWORD is set"
+        Write-Host "Trying to get token from common user Student000..."
         exit 0
     }
     else {
-        echo "Error: DEFECTDOJO_TOKEN is not set. Please set the secret in the dev environment."
-        echo "Error: DEFECTDOJO_COMMONPASSWORD is not set. Please set the secret in the dev environment."
-        echo "You need to set either DEFECTDOJO_TOKEN or DEFECTDOJO_COMMONPASSWORD to run the pipeline."
+        Write-Host "Error: DEFECTDOJO_TOKEN is not set. Please set the secret in the dev environment."
+        Write-Host "Error: DEFECTDOJO_COMMONPASSWORD is not set. Please set the secret in the dev environment."
+        Write-Host "You need to set either DEFECTDOJO_TOKEN or DEFECTDOJO_COMMONPASSWORD to run the pipeline."
         exit 1
     }
 }
