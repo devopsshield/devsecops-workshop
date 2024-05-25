a2729d71e95dcf24dc42fe1d94a5ef81ecd1f51e
Emmanuel.Knafo <Emmanuel.Knafo@cad4devops.com>
2024-05-25 16:26:58 -0400

    try to fetch

--- labs/scripts/trickyLogic.ps1
+++ labs/scripts/trickyLogic.ps1
@@ -13,8 +13,25 @@ else {
     Write-Host "checking for DEFECTDOJO_COMMONPASSWORD"
     if ($defectDojoCommonPasswordIsSet) {
         Write-Host "DEFECTDOJO_COMMONPASSWORD is set"
-        Write-Host "Trying to get token from common user Student000..."
-        exit 0
+        Write-Host "Trying to get token from common user Student000..."        
+        # get token from common user
+        $DEFECTDOJO_TOKEN_COMMON = (Invoke-RestMethod -Method Post -Uri "${env:DEFECTDOJO_URL}/api-token-auth/" `
+                -ContentType 'application/json' `
+                -Body (@{
+                    "username" = "$env:DEFECTDOJO_COMMONUSER";
+                    "password" = "$env:DEFECTDOJO_COMMONPASSWORD"
+                } | ConvertTo-Json)).token
+
+        if ($env:DEFECTDOJO_TOKEN_COMMON) {
+            # Write-Host "DEFECTDOJO_TOKEN: $env:DEFECTDOJO_TOKEN_COMMON"
+            Write-Host "DEFECTDOJO_TOKEN is set"
+            Add-Content -Path $env:GITHUB_ENV -Value "DEFECTDOJO_TOKEN=$env:DEFECTDOJO_TOKEN_COMMON"
+            exit 0            
+        }
+        else {
+            Write-Host "Error: Failed to get token from common user ${env:DEFECTDOJO_COMMONUSER}. Please check the credentials."
+            exit 1
+        }
     }
     else {
         Write-Host "Error: DEFECTDOJO_TOKEN is not set. Please set the secret in the dev environment."
