8fae2284561293b4eb775be50c593731966da7eb
Emmanuel.Knafo <Emmanuel.Knafo@cad4devops.com>
2024-05-25 16:51:17 -0400

    try this

--- labs/scripts/trickyLogic.ps1
+++ labs/scripts/trickyLogic.ps1
@@ -13,7 +13,7 @@ else {
     Write-Host "checking for DEFECTDOJO_COMMONPASSWORD"
     if ($defectDojoCommonPasswordIsSet) {
         Write-Host "DEFECTDOJO_COMMONPASSWORD is set"
-        Write-Host "Trying to get token from common user Student000..."        
+        Write-Host "Trying to get token from common user Student000..."
         # get token from common user
         $DEFECTDOJO_TOKEN_COMMON = (Invoke-RestMethod -Method Post -Uri "${env:DEFECTDOJO_URL}/api-token-auth/" `
                 -ContentType 'application/json' `
@@ -25,8 +25,26 @@ else {
         if ($env:DEFECTDOJO_TOKEN_COMMON) {
             # Write-Host "DEFECTDOJO_TOKEN: $env:DEFECTDOJO_TOKEN_COMMON"
             Write-Host "DEFECTDOJO_TOKEN is set"
-            Add-Content -Path $env:GITHUB_ENV -Value "DEFECTDOJO_TOKEN=$env:DEFECTDOJO_TOKEN_COMMON"
-            exit 0            
+            #Add-Content -Path $env:GITHUB_ENV -Value "DEFECTDOJO_TOKEN=$env:DEFECTDOJO_TOKEN_COMMON"
+
+            # now to fetch the product id
+            # Get the product id
+            $DEFECTDOJO_PRODUCTID_COMMON = Invoke-RestMethod -Method Get -Uri "${env:DEFECTDOJO_URL}/products/?name=${env:DEFECTDOJO_COMMONPRODUCTNAME}" `
+                -Headers @{ "Authorization" = "Token ${env:DEFECTDOJO_TOKEN}" } | Select-Object -ExpandProperty results[0].id
+
+            if (-not [string]::IsNullOrWhiteSpace($DEFECTDOJO_PRODUCTID_COMMON)) {
+                Write-Host "DEFECTDOJO_PRODUCTID: $DEFECTDOJO_PRODUCTID_COMMON"
+                Write-Host "DEFECTDOJO_PRODUCTID is set"
+                Add-Content -Path $env:GITHUB_ENV -Value "DEFECTDOJO_PRODUCTID=$DEFECTDOJO_PRODUCTID_COMMON"
+                exit 0
+            }
+            else {
+                Write-Host "Error: Failed to get product id for ${env:DEFECTDOJO_COMMONPRODUCTNAME}. Please check the product name."
+                exit 1
+            }
+
+
+            exit 0
         }
         else {
             Write-Host "Error: Failed to get token from common user ${env:DEFECTDOJO_COMMONUSER}. Please check the credentials."
